d7ae081b082bffdae918b49add3c759bec84a50f
Kent Avasarala <kent_92@live.com>
2019-09-14 20:54:02 -0700

    Some experiment

--- app.py
+++ app.py
@@ -1,8 +1,22 @@
+from numbers_qwe import *
+from math import *
 
 
 def main():
     print("Hello, World!")
 
 
+def solve_problem(a: int, b: int) -> int:
+    c = multiply_two_numbers(a, b)
+    return a + b + c
+
+
+def calculate_circle_area(radius) -> object:
+    return pi * radius**2
+
+
 if __name__ == '__main__':
+    print(solve_problem(4, 5))
+    print(divide_two_numbers(5, 4))
+    print(calculate_circle_area(2.6317))
     main()
---
+++ numbers_qwe.py
@@ -0,0 +1,6 @@
+def multiply_two_numbers(x, y):
+    return x * y
+
+
+def divide_two_numbers(x, y):
+    return x / y
